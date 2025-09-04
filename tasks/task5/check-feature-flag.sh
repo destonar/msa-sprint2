738073b78840587f935854c3d6ed4174122f47da
destonar <destonar@gmail.com>
2025-09-05 00:06:17 +0300

    Finish task 5

--- tasks/task5/check-feature-flag.sh
+++ tasks/task5/check-feature-flag.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-echo "▶️ Проверка Feature Flag (X-Feature-Enabled: true)..."
+echo "▶️ Проверка Feature Flag (X-KafkaPublish-Enabled: true)..."
 
 # Отправляем запрос с заголовком, чтобы маршрутизировать трафик на `v2`
-curl -H "X-Feature-Enabled: true" http://localhost:9090/ping
+curl -H "X-KafkaPublish-Enabled: true" http://localhost:9090/ping
